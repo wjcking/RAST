2fb0822b77f18c65dc9ee3b5253ef840626e1e99
wjcking <wjcking@hotmail.com>
2021-01-14 13:28:43 +0800

    #def

---
+++ HyperRanket.h
@@ -0,0 +1,25 @@
+#pragma once
+/*
+*Basic typedef oragnizing
+*/
+
+
+/*
+*The DirectX Utility Library DXUT 
+*/
+#ifdef DXUT_H
+
+ typedef CModelViewerCamera    ModelViewerCamera;
+ typedef CDXUTDialogResourceManager   DialogResourceManager;
+ typedef CD3DSettingsDlg        SettingsDialog;
+#endif
+
+ ////DXUT Optional SDKMISC_H sdkmisc.h
+//typedef CDXUTTextHelper*   TextHelper
+//typedef CDXUTDialog        Dialog
+//
+////  d3dx9.h resources 
+//ID3DXFont*        
+//ID3DXSprite*          
+//ID3DXEffect*             
+//D3DXHANDLE    
--- SimpleSample.cpp
+++ SimpleSample.cpp
@@ -12,6 +12,7 @@
 #include "SDKmesh.h"
 #include "resource.h"
 
+#include "HyperRanket.h"
 //#define DEBUG_VS   // Uncomment this line to debug D3D9 vertex shaders 
 //#define DEBUG_PS   // Uncomment this line to debug D3D9 pixel shaders 
 
@@ -19,20 +20,20 @@
 //--------------------------------------------------------------------------------------
 // Global variables
 //--------------------------------------------------------------------------------------
-CModelViewerCamera          g_Camera;               // A model viewing camera
-CDXUTDialogResourceManager  g_DialogResourceManager; // manager for shared resources of dialogs
-CD3DSettingsDlg             g_SettingsDlg;          // Device settings dialog
-CDXUTTextHelper*            g_pTxtHelper = NULL;
-CDXUTDialog                 g_HUD;                  // dialog for standard controls
-CDXUTDialog                 g_SampleUI;             // dialog for sample specific controls
+ModelViewerCamera          Camera;               // A model viewing camera
+CDXUTDialogResourceManager  resourceManager; // manager for shared resources of dialogs
+CD3DSettingsDlg             settingDialog;          // Device settings dialog
+CDXUTTextHelper*            textHelper = NULL;
+CDXUTDialog                 HUD;                  // dialog for standard controls
+CDXUTDialog                 AUI;             // dialog for sample specific controls
 
 // Direct3D 9 resources
-ID3DXFont*                  g_pFont9 = NULL;
-ID3DXSprite*                g_pSprite9 = NULL;
-ID3DXEffect*                g_pEffect9 = NULL;
-D3DXHANDLE                  g_hmWorldViewProjection;
-D3DXHANDLE                  g_hmWorld;
-D3DXHANDLE                  g_hfTime;
+ID3DXFont*                  Font = NULL;
+ID3DXSprite*                Sprite = NULL;
+ID3DXEffect*                Effect = NULL;
+D3DXHANDLE                  WorldProjection;
+D3DXHANDLE                  World;
+D3DXHANDLE                  TimeHandler;
 
 //--------------------------------------------------------------------------------------
 // UI control IDs
@@ -109,16 +110,16 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
 //--------------------------------------------------------------------------------------
 void InitApp()
 {
-    g_SettingsDlg.Init( &g_DialogResourceManager );
-    g_HUD.Init( &g_DialogResourceManager );
-    g_SampleUI.Init( &g_DialogResourceManager );
+    settingDialog.Init( &resourceManager );
+    HUD.Init( &resourceManager );
+    AUI.Init( &resourceManager );
 
-    g_HUD.SetCallback( OnGUIEvent ); int iY = 10;
-    g_HUD.AddButton( IDC_TOGGLEFULLSCREEN, L"Toggle full screen", 35, iY, 125, 22 );
-    g_HUD.AddButton( IDC_TOGGLEREF, L"Toggle REF (F3)", 35, iY += 24, 125, 22, VK_F3 );
-    g_HUD.AddButton( IDC_CHANGEDEVICE, L"Change device (F2)", 35, iY += 24, 125, 22, VK_F2 );
+    HUD.SetCallback( OnGUIEvent ); int iY = 10;
+    HUD.AddButton( IDC_TOGGLEFULLSCREEN, L"Toggle full screen", 35, iY, 125, 22 );
+    HUD.AddButton( IDC_TOGGLEREF, L"Toggle REF (F3)", 35, iY += 24, 125, 22, VK_F3 );
+    HUD.AddButton( IDC_CHANGEDEVICE, L"Change device (F2)", 35, iY += 24, 125, 22, VK_F2 );
 
-    g_SampleUI.SetCallback( OnGUIEvent ); iY = 10;
+    AUI.SetCallback( OnGUIEvent ); iY = 10;
 }
 
 
@@ -128,12 +129,12 @@ void InitApp()
 //--------------------------------------------------------------------------------------
 void RenderText()
 {
-    g_pTxtHelper->Begin();
-    g_pTxtHelper->SetInsertionPos( 5, 5 );
-    g_pTxtHelper->SetForegroundColor( D3DXCOLOR( 1.0f, 1.0f, 0.0f, 1.0f ) );
-    g_pTxtHelper->DrawTextLine( DXUTGetFrameStats( DXUTIsVsyncEnabled() ) );
-    g_pTxtHelper->DrawTextLine( DXUTGetDeviceStats() );
-    g_pTxtHelper->End();
+    textHelper->Begin();
+    textHelper->SetInsertionPos( 5, 5 );
+    textHelper->SetForegroundColor( D3DXCOLOR( 1.0f, 1.0f, 0.0f, 1.0f ) );
+    textHelper->DrawTextLine( DXUTGetFrameStats( DXUTIsVsyncEnabled() ) );
+    textHelper->DrawTextLine( DXUTGetDeviceStats() );
+    textHelper->End();
 }
 
 
@@ -217,12 +218,12 @@ HRESULT CALLBACK OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice, const D3DSURF
 {
     HRESULT hr;
 
-    V_RETURN( g_DialogResourceManager.OnD3D9CreateDevice( pd3dDevice ) );
-    V_RETURN( g_SettingsDlg.OnD3D9CreateDevice( pd3dDevice ) );
+    V_RETURN( resourceManager.OnD3D9CreateDevice( pd3dDevice ) );
+    V_RETURN( settingDialog.OnD3D9CreateDevice( pd3dDevice ) );
 
     V_RETURN( D3DXCreateFont( pd3dDevice, 15, 0, FW_BOLD, 1, FALSE, DEFAULT_CHARSET,
                               OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE,
-                              L"Arial", &g_pFont9 ) );
+                              L"Arial", &Font ) );
 
     // Read the D3DX effect file
     WCHAR str[MAX_PATH];
@@ -240,16 +241,16 @@ HRESULT CALLBACK OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice, const D3DSURF
 
     V_RETURN( DXUTFindDXSDKMediaFileCch( str, MAX_PATH, L"SimpleSample.fx" ) );
     V_RETURN( D3DXCreateEffectFromFile( pd3dDevice, str, NULL, NULL, dwShaderFlags,
-                                        NULL, &g_pEffect9, NULL ) );
+                                        NULL, &Effect, NULL ) );
 
-    g_hmWorldViewProjection = g_pEffect9->GetParameterByName( NULL, "g_mWorldViewProjection" );
-    g_hmWorld = g_pEffect9->GetParameterByName( NULL, "g_mWorld" );
-    g_hfTime = g_pEffect9->GetParameterByName( NULL, "g_fTime" );
+    WorldProjection = Effect->GetParameterByName( NULL, "g_mWorldViewProjection" );
+    World = Effect->GetParameterByName( NULL, "g_mWorld" );
+    TimeHandler = Effect->GetParameterByName( NULL, "g_fTime" );
 
     // Setup the camera's view parameters
     D3DXVECTOR3 vecEye( 0.0f, 0.0f, -5.0f );
     D3DXVECTOR3 vecAt ( 0.0f, 0.0f, -0.0f );
-    g_Camera.SetViewParams( &vecEye, &vecAt );
+    Camera.SetViewParams( &vecEye, &vecAt );
 
     return S_OK;
 }
@@ -264,24 +265,24 @@ HRESULT CALLBACK OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice,
 {
     HRESULT hr;
 
-    V_RETURN( g_DialogResourceManager.OnD3D9ResetDevice() );
-    V_RETURN( g_SettingsDlg.OnD3D9ResetDevice() );
+    V_RETURN( resourceManager.OnD3D9ResetDevice() );
+    V_RETURN( settingDialog.OnD3D9ResetDevice() );
 
-    if( g_pFont9 ) V_RETURN( g_pFont9->OnResetDevice() );
-    if( g_pEffect9 ) V_RETURN( g_pEffect9->OnResetDevice() );
+    if( Font ) V_RETURN( Font->OnResetDevice() );
+    if( Effect ) V_RETURN( Effect->OnResetDevice() );
 
-    V_RETURN( D3DXCreateSprite( pd3dDevice, &g_pSprite9 ) );
-    g_pTxtHelper = new CDXUTTextHelper( g_pFont9, g_pSprite9, NULL, NULL, 15 );
+    V_RETURN( D3DXCreateSprite( pd3dDevice, &Sprite ) );
+    textHelper = new CDXUTTextHelper( Font, Sprite, NULL, NULL, 15 );
 
     // Setup the camera's projection parameters
     float fAspectRatio = pBackBufferSurfaceDesc->Width / ( FLOAT )pBackBufferSurfaceDesc->Height;
-    g_Camera.SetProjParams( D3DX_PI / 4, fAspectRatio, 0.1f, 1000.0f );
-    g_Camera.SetWindow( pBackBufferSurfaceDesc->Width, pBackBufferSurfaceDesc->Height );
+    Camera.SetProjParams( D3DX_PI / 4, fAspectRatio, 0.1f, 1000.0f );
+    Camera.SetWindow( pBackBufferSurfaceDesc->Width, pBackBufferSurfaceDesc->Height );
 
-    g_HUD.SetLocation( pBackBufferSurfaceDesc->Width - 170, 0 );
-    g_HUD.SetSize( 170, 170 );
-    g_SampleUI.SetLocation( pBackBufferSurfaceDesc->Width - 170, pBackBufferSurfaceDesc->Height - 350 );
-    g_SampleUI.SetSize( 170, 300 );
+    HUD.SetLocation( pBackBufferSurfaceDesc->Width - 170, 0 );
+    HUD.SetSize( 170, 170 );
+    AUI.SetLocation( pBackBufferSurfaceDesc->Width - 170, pBackBufferSurfaceDesc->Height - 350 );
+    AUI.SetSize( 170, 300 );
 
     return S_OK;
 }
@@ -293,7 +294,7 @@ HRESULT CALLBACK OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice,
 void CALLBACK OnFrameMove( double fTime, float fElapsedTime, void* pUserContext )
 {
     // Update the camera's position based on user input 
-    g_Camera.FrameMove( fElapsedTime );
+    Camera.FrameMove( fElapsedTime );
 }
 
 
@@ -309,9 +310,9 @@ void CALLBACK OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, flo
     D3DXMATRIXA16 mWorldViewProjection;
 
     // If the settings dialog is being shown, then render it instead of rendering the app's scene
-    if( g_SettingsDlg.IsActive() )
+    if( settingDialog.IsActive() )
     {
-        g_SettingsDlg.OnRender( fElapsedTime );
+        settingDialog.OnRender( fElapsedTime );
         return;
     }
 
@@ -322,23 +323,23 @@ void CALLBACK OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, flo
     if( SUCCEEDED( pd3dDevice->BeginScene() ) )
     {
         // Get the projection & view matrix from the camera class
-        mWorld = *g_Camera.GetWorldMatrix();
-        mProj = *g_Camera.GetProjMatrix();
-        mView = *g_Camera.GetViewMatrix();
+        mWorld = *Camera.GetWorldMatrix();
+        mProj = *Camera.GetProjMatrix();
+        mView = *Camera.GetViewMatrix();
 
         mWorldViewProjection = mWorld * mView * mProj;
 
         // Update the effect's variables.  Instead of using strings, it would 
         // be more efficient to cache a handle to the parameter by calling 
         // ID3DXEffect::GetParameterByName
-        V( g_pEffect9->SetMatrix( g_hmWorldViewProjection, &mWorldViewProjection ) );
-        V( g_pEffect9->SetMatrix( g_hmWorld, &mWorld ) );
-        V( g_pEffect9->SetFloat( g_hfTime, ( float )fTime ) );
+        V( Effect->SetMatrix( WorldProjection, &mWorldViewProjection ) );
+        V( Effect->SetMatrix( World, &mWorld ) );
+        V( Effect->SetFloat( TimeHandler, ( float )fTime ) );
 
         DXUT_BeginPerfEvent( DXUT_PERFEVENTCOLOR, L"HUD / Stats" ); // These events are to help PIX identify what the code is doing
         RenderText();
-        V( g_HUD.OnRender( fElapsedTime ) );
-        V( g_SampleUI.OnRender( fElapsedTime ) );
+        V( HUD.OnRender( fElapsedTime ) );
+        V( AUI.OnRender( fElapsedTime ) );
         DXUT_EndPerfEvent();
 
         V( pd3dDevice->EndScene() );
@@ -353,27 +354,27 @@ LRESULT CALLBACK MsgProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, bo
                           void* pUserContext )
 {
     // Pass messages to dialog resource manager calls so GUI state is updated correctly
-    *pbNoFurtherProcessing = g_DialogResourceManager.MsgProc( hWnd, uMsg, wParam, lParam );
+    *pbNoFurtherProcessing = resourceManager.MsgProc( hWnd, uMsg, wParam, lParam );
     if( *pbNoFurtherProcessing )
         return 0;
 
     // Pass messages to settings dialog if its active
-    if( g_SettingsDlg.IsActive() )
+    if( settingDialog.IsActive() )
     {
-        g_SettingsDlg.MsgProc( hWnd, uMsg, wParam, lParam );
+        settingDialog.MsgProc( hWnd, uMsg, wParam, lParam );
         return 0;
     }
 
     // Give the dialogs a chance to handle the message first
-    *pbNoFurtherProcessing = g_HUD.MsgProc( hWnd, uMsg, wParam, lParam );
+    *pbNoFurtherProcessing = HUD.MsgProc( hWnd, uMsg, wParam, lParam );
     if( *pbNoFurtherProcessing )
         return 0;
-    *pbNoFurtherProcessing = g_SampleUI.MsgProc( hWnd, uMsg, wParam, lParam );
+    *pbNoFurtherProcessing = AUI.MsgProc( hWnd, uMsg, wParam, lParam );
     if( *pbNoFurtherProcessing )
         return 0;
 
     // Pass all remaining windows messages to camera so it can respond to user input
-    g_Camera.HandleMessages( hWnd, uMsg, wParam, lParam );
+    Camera.HandleMessages( hWnd, uMsg, wParam, lParam );
 
     return 0;
 }
@@ -399,7 +400,7 @@ void CALLBACK OnGUIEvent( UINT nEvent, int nControlID, CDXUTControl* pControl, v
         case IDC_TOGGLEREF:
             DXUTToggleREF(); break;
         case IDC_CHANGEDEVICE:
-            g_SettingsDlg.SetActive( !g_SettingsDlg.IsActive() ); break;
+            settingDialog.SetActive( !settingDialog.IsActive() ); break;
     }
 }
 
@@ -409,12 +410,12 @@ void CALLBACK OnGUIEvent( UINT nEvent, int nControlID, CDXUTControl* pControl, v
 //--------------------------------------------------------------------------------------
 void CALLBACK OnD3D9LostDevice( void* pUserContext )
 {
-    g_DialogResourceManager.OnD3D9LostDevice();
-    g_SettingsDlg.OnD3D9LostDevice();
-    if( g_pFont9 ) g_pFont9->OnLostDevice();
-    if( g_pEffect9 ) g_pEffect9->OnLostDevice();
-    SAFE_RELEASE( g_pSprite9 );
-    SAFE_DELETE( g_pTxtHelper );
+    resourceManager.OnD3D9LostDevice();
+    settingDialog.OnD3D9LostDevice();
+    if( Font ) Font->OnLostDevice();
+    if( Effect ) Effect->OnLostDevice();
+    SAFE_RELEASE( Sprite );
+    SAFE_DELETE( textHelper );
 }
 
 
@@ -423,10 +424,10 @@ void CALLBACK OnD3D9LostDevice( void* pUserContext )
 //--------------------------------------------------------------------------------------
 void CALLBACK OnD3D9DestroyDevice( void* pUserContext )
 {
-    g_DialogResourceManager.OnD3D9DestroyDevice();
-    g_SettingsDlg.OnD3D9DestroyDevice();
-    SAFE_RELEASE( g_pEffect9 );
-    SAFE_RELEASE( g_pFont9 );
+    resourceManager.OnD3D9DestroyDevice();
+    settingDialog.OnD3D9DestroyDevice();
+    SAFE_RELEASE( Effect );
+    SAFE_RELEASE( Font );
 }
 
 
